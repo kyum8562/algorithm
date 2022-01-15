ceae7fece5f6c4146760598b97c1de0ab8489ccb
kyum8562@gmail.com <66922359+kyum8562@users.noreply.github.com>
2022-01-15 18:24:15 +0900

    0115-1

---
+++ sorting/merge.js
@@ -0,0 +1,14 @@
+function solution(arr){
+    // Merge Sort
+    
+    var arr = arr;
+}
+
+function load(){
+    var result = solution(['7', '3', '1', '5', '6', '4', '2']);
+    console.log(result);
+}
+function init(){
+    load();
+}
+init();
---
+++ sorting/quickSelect.js
@@ -0,0 +1,37 @@
+function solution(arr){   
+    var arr = arr;
+    var pivot = arr.length-2;
+    var p1 = 0;
+    var p2 = arr.length-1;
+    console.log(`prevArr : ${arr}`);
+
+    var tmp = arr[pivot];
+    arr[pivot] = arr[p2];
+    arr[p2] = tmp;
+    console.log(`currArr : ${arr}`);
+
+    tmp = p2;
+    p2 = pivot;
+    pivot = tmp;
+
+    for( var i = 0 ; i < arr.length ; i ++){
+        // p1
+        if(arr[p1] < arr[pivot]){
+            p1++;
+        }
+        else{
+            break;
+        }
+        
+    }
+    return p1;
+}   
+
+function load(){
+    var result = solution(['3', '5', '9', '1', '2', '4', '7']);
+    console.log(result);
+}
+function init(){
+    load();
+}
+init();
