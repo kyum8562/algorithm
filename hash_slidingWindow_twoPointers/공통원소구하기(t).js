189eb9ad7c79226722a583aaed40de332ea6df6d
kyum8562@gmail.com <66922359+kyum8562@users.noreply.github.com>
2022-03-28 23:57:43 +0900

    0328

---
+++ hash_slidingWindow_twoPointers/공통원소구하기(t).js
@@ -0,0 +1,11 @@
+/*
+공통원소 구하기
+A, B 두 개의 집합이 주어지면 두 집합의 공통 원소를 추출하여 
+오름차순으로 출력하는 프로그램을 작성하세요
+*/
+function solution(arr1, arr2){
+    let answer = [];
+
+    return answer;
+}
+console.log(solution([1, 3, 9, 5, 2], [3, 2, 5, 7, 8]))
---
+++ hash_slidingWindow_twoPointers/두배열합치기(t).js
@@ -0,0 +1,35 @@
+/*
+두 배열 합치기
+오름차순으로 정렬(nLogn)이 된 두 배열이 주어지면 
+두 배열을 오름차순으로 합쳐 출력하는 프로그램을 작성하세요.
+*/
+function solution(arr1, arr2) {
+    let answer = [];
+    let p1 = p2 = 0;
+    let allLength =  arr1.length + arr2.length;
+    while(answer.length < allLength){
+       if(arr1[p1] >arr2[p2]){
+           if(p2 < arr2.length){
+                answer.push(arr2[p2++]);
+            }
+       }
+       else if(arr1[p1] < arr2[p2]){
+            if(p1 < arr1.length){
+                answer.push(arr1[p1++]);
+            }
+       }
+       else{
+
+           if(p1 < arr1.length){
+            answer.push(arr1[p1++]);
+           } 
+           if(p2 < arr2.length){
+            answer.push(arr2[p2++]);
+           } 
+       }
+    }
+    return answer;
+}
+
+console.log(solution([1, 3, 5], [2, 3, 6, 7, 9]));
+1, 2, 3, 3, 5
