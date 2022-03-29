cdb40844c761e6030e3d3e23b848f6836b1ffeb1
kyum8562@gmail.com <66922359+kyum8562@users.noreply.github.com>
2022-03-29 23:01:41 +0900

    0329

--- hash_slidingWindow_twoPointers/연속부분수열1(t).js
+++ hash_slidingWindow_twoPointers/연속부분수열1(t).js
@@ -5,25 +5,39 @@ N개의 수로 이루어진 수열이 주어집니다.
 만약 m=6이고 수열이 다음과 같다면
 1 2 1 3 1 1 1 2 합이 6이 되는 연속 부분수열은 
 {2, 1, 3}, {1, 3, 1, 1}, {3, 1, 1, 1}로 총 3가지이다.
+
+투 포인터는 시간복잡도 O(m)이다.
 */
 function solution(m, arr){
-    let answer= 0;
-    let p1 = p2 =0;
-    let sum = arr[p1];
-    while(p2 !== arr.length - 1){
-        if(sum === m){
-            answer++;
-            sum -= arr[p1++];
-        }
-        else if(sum < m){
-            sum += arr[++p2];
-        }
-        else if(sum > m){
-            sum -= arr[p1++];
-        }
-    }
+    // (1)
+    // let answer= 0;
+    // let p1 = p2 =0;
+    // let sum = arr[p1];
+    // while(p2 !== arr.length - 1){
+    //     if(sum === m){
+    //         answer++;
+    //         sum -= arr[p1++];
+    //     }
+    //     else if(sum < m){
+    //         sum += arr[++p2];
+    //     }
+    //     else if(sum > m){
+    //         sum -= arr[p1++];
+    //     }
+    // }
     
-
+    // (2)
+    let answer = 0;
+    let lt = 0;
+    let sum = 0;
+    for(let rt = 0 ; rt < arr.length ; rt ++){
+        sum += arr[rt];
+        while(sum > m){
+            sum -= arr[lt++];
+        }
+        if(sum === m) answer++;
+    }
+        
     return answer;
 }
 console.log(solution(6, [1, 2, 1, 3, 1, 1, 1, 2]));
---
+++ hash_slidingWindow_twoPointers/최대매출(s).js
@@ -0,0 +1,21 @@
+/*
+최대 매출
+현수의 아빠는 제과점을 운영한다. 현수 아빠는 현수에게 N일동안의 매출기록을 주고
+연속된 K일 동안의 최대 매출액이 얼마인지 구하라고 했다.
+만약 N=10이라면, 10일간의 매출기록은 다음과 같다. [12, 15, 11, 20, 25, 10, 20, 19, 13, 15]
+이 때 K=3이라면, 연속된 3일간의 최대 매출액은 11+20+25 = 56만원이다.
+
+슬라이딩 윈도우는 O(N)이다.
+*/
+function solution(k, arr){
+    let answer = sum = 0;
+    for(let i = 0 ; i < k ; i ++) sum += arr[i];
+    
+    for(let i = k ; i < arr.length ; i ++){
+        if(answer < sum) answer = sum;
+        sum += (arr[i] - arr[i-k]);
+    }
+    
+    return answer;
+}
+console.log(solution(3, [12, 15, 11, 20, 25, 10, 20, 19, 13, 15]));
