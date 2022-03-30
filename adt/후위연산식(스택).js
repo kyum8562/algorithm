5d650cc92eb4492d09618cedea8c2cfcf759a33e
kyum8562@gmail.com <66922359+kyum8562@users.noreply.github.com>
2022-03-31 00:27:52 +0900

    0331

---
+++ adt/후위연산식(스택).js
@@ -0,0 +1,28 @@
+/*
+후위연산식이 주어지면 연산한 결과를 출력하는 프로그램을 작성하세요.
+만약 3*(5+2)-9을 후위연산식으로 표현하면 352+*9- 이며 답은 12이다.
+
+후위연산식 -> 중위연산식으로 변경시
+stack에서 먼저나온수가 rt, 나중에 나온수가 lt로 지정하여 계산해야 한다.
+*/
+function solution(str){
+    let answer = [];
+
+    for(let i = 0 ; i < str.length ; i ++){
+        if(str[i]/3) {
+            answer.push(Number(str[i]));
+        }
+        else {
+            let tmp1 = answer.pop();
+            let tmp2 = answer.pop();
+            if(str[i] === '+') answer.push(tmp2+tmp1);
+            else if(str[i] === '*') answer.push(tmp2*tmp1);
+            else if(str[i] === '/') answer.push(tmp2/tmp1);
+            else if(str[i] === '-') answer.push(tmp2-tmp1);
+        }
+    }
+
+
+    return answer;
+}
+console.log(solution('352+*9-'));
