c64188db9570647aee77ab232c71499c3bb06215
kyum8562@gmail.com <66922359+kyum8562@users.noreply.github.com>
2022-05-03 20:12:50 +0900

    0503

--- 신규아이디추천.js
+++ 신규아이디추천.js
@@ -25,44 +25,95 @@ return 하도록 solution 함수를 완성해 주세요.
 */
 
 function solution(new_id){
-    // 1단계
-    let new_id_Lower = new_id.toLowerCase();
-    let answer = [];
-    let a = 0;
-    for(let i = 0 ; i <new_id.length ; i ++){
-        // 2단계
-        let tmp = new_id_Lower[i].charCodeAt();
-        if(!(tmp === 46 ||
-             tmp === 45 ||
-             tmp === 95 ||
-             tmp >= 97 &&
-             tmp <= 122 ||
-             tmp >= 48 &&
-             tmp <= 57)){
-                answer.push(i);
-                a++;
-             }
-    }
-    let right = new_id_Lower.split("");
-    if(answer.length) right.splice(answer[0],1);
-    for(let i = 1 ; i < answer.length; i ++){
-        right.splice(answer[i]-1,1);
-    }
-    // 3단계
-    for(let i = 0 ; i < right.length -1; i++){
-        if(right[i] === '.' && right[i+1] === '.' ){
-            right.splice(answer[i],1);
-            i -= 1;
-        }
-    }
-    console.log(right)
-    console.log(new_id_Lower.length, a, right.length)
-//    return a;
+    // // 1단계 대문자를 소문자로 변환
+    // let new_id_Lower = new_id.toLowerCase();
+    // // 소문자로 변경된 문자열을 배열로 치환
+    // let answer = new_id_Lower.split('');
+    // for(let i = 0 ; i < answer.length ; i ++){
+    //     // 4단계(1) : 마침표(.)가 처음에 위치한다면 제거합니다.
+    //     while(i === 0 && answer[0] === '.'){
+    //         answer.shift();
+    //     }
+    //     // 4단계(2) : 마침표(.)가 마지막에 위치한다면 제거합니다.
+    //     while(answer[answer.length-1] === '.'){
+    //         answer.pop();
+    //     }
+    //     if(answer.length >= 1){
+    //         // 2단계 : 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
+    //         let tmp = answer[i].charCodeAt();
+    //         if(!(tmp === 46 || // '.'
+    //                 tmp === 45 || // '-'
+    //                 tmp === 95 || // '_'
+    //                 tmp >= 97 && // 소문자
+    //                 tmp <= 122 ||
+    //                 tmp >= 48 && // 숫자
+    //                 tmp <= 57))
+    //             {
+    //                 answer[i] = '';
+    //             }
+    
+    //         // 3단계 : 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
+    //         if(answer[i] ==='.' && answer[i+1] === '.'){
+    //             answer[i] = '';
+    //             i = -1;
+    //         }
+    //     }
+    // }
+    
+    // // 빈공간 제거 및 배열 -> 문자열로 변환
+    // answer = answer.filter((val, idx) => answer[idx] !== '').join('');
+
+    // while(answer[answer.length-1] === '.'){
+    //     answer = answer.slice(0, answer.length-1);
+    // }
+
+    // // 5단계 빈 문자열이라면, new_id에 "a"를 대입합니다.
+    // if(answer.length === 0){
+    //     answer = 'a';
+    // }
+    // // 6단계
+    // while(answer.length > 15){
+    //     answer = answer.slice(0, 15);
+
+    //     // 마침표(.)가 마지막에 위치한다면 제거합니다.
+    //     while(answer[answer.length-1] === '.'){
+    //         answer = answer.slice(0, answer.length-1);
+    //     }
+    // }
+    // if(answer.length <= 2){
+    //     if(answer.length === 1) answer += answer[answer.length-1].repeat(2);
+    //     else if(answer.length === 2) answer += answer[answer.length-1].repeat(1);
+    // }
+    // console.log("");
+    // console.log(`before id : ${new_id}`);
+    // console.log(`before id.length : ${new_id.length}`);
+    // console.log(`after id : ${answer}`);
+    // console.log(`after id.length : ${answer.length}`);
+
+    // return answer;
 }
 
+function solution1(new_id){
+    const answer = new_id
 
-console.log(solution("...!@BaT#*..y.abcdefghijklm"));
+    .toLowerCase() // 1
+    .replace(/[^\w-_.]/g, '') // 2
+    .replace(/\.+/g, '.') // 3
+    .replace(/^\.|\.$/g, '') // 4
+    .replace(/^$/, 'a') // 5
+    .slice(0, 15).replace(/\.$/, ''); // 6
+    
+    const len = answer.length;
+
+    return len > 2 ? 
+        answer :
+        answer + answer.charAt(len - 1).repeat(3 - len);
+}
+
+// console.log(solution("...!@BaT#*..y.abcdefghijklm"));
+// console.log(solution("."));
+console.log(solution1("==="));
 // console.log(solution("z-+.^."));
 // console.log(solution("=.="));
 // console.log(solution("123_.def"));
-// console.log(solution("abcdefghijklmn.p"));
+// console.log(solution("abcdefghijklmn.p")); //abcdefghijklmn
---
+++ 음양더하기.js
@@ -0,0 +1,19 @@
+/*
+음양 더하기
+어떤 정수들의 절댓값을 차례대로 담은 정수 배열 absolutes와 
+이 정수들의 부호를 차례대로 담은 불리언 배열 signs가 매개변수로 
+주어집니다. 실제 정수들의 합을 구하여 
+return 하도록 solution 함수를 완성해주세요.
+*/
+function solution(absolutes, signs){
+    // let answer = 0;
+    // for(let i = 0 ; i < signs.length ; i++) if(signs[i] === false) absolutes[i] = -absolutes[i];
+    // absolutes.filter((x, i) => answer +=absolutes[i]);
+    
+    // return answer;
+    
+    return absolutes.reduce((acc, val, idx) => acc + val*(signs[idx] ? 1 : -1), 0)
+}
+
+
+console.log(solution([4,7,12], [true,false,true]));
