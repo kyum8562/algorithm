897c4a7d88a8de5fd4a41368eb8bc745b372979e
giraffe_kim <kyum8562@gmail.com>
2023-12-29 12:58:13 +0900

    [Silver V] Title: 귀찮아 (SIB), Time: 292 ms, Memory: 24620 KB -BaekjoonHub

---
+++ 백준/Silver/14929. 귀찮아 （SIB）/귀찮아 （SIB）.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] prefixSum = new int[N+1]; // 패딩
+        int[] map = new int[N+1]; // 패딩
+        for (int i = 1; i <= N; i++) {
+            map[i] = Integer.parseInt(st.nextToken());
+            prefixSum[i] = prefixSum[i-1] + map[i];
+        }
+        long sum = 0;
+        for (int i = 1; i <= N ; i++) {
+            sum += (prefixSum[N] - prefixSum[i])*map[i];
+        }
+        System.out.println(sum);
+    }
+}
