47bfc4516bf6f111e8e6bc1ea61f9ba0aa46e3c1
giraffe_kim <kyum8562@gmail.com>
2023-12-31 17:02:06 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 224 ms, Memory: 19240 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -4,7 +4,6 @@ import java.util.*;
 public class Main {
     static int N;
     static int[][] map;
-    static int[][] dist;
     static int INF = Integer.MAX_VALUE;
     static int[] dr = {-1, 0, 1, 0};
     static int[] dc = {0, 1, 0, -1};
@@ -12,60 +11,63 @@ public class Main {
     static class Node {
         int r;
         int c;
-        int dist;
+        int cost;
 
-        public Node(int r, int c, int dist) {
+        public Node(int r, int c, int cost) {
             this.r = r;
             this.c = c;
-            this.dist = dist;
+            this.cost = cost;
         }
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int cnt = 1;
         while (true) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
-            if(N == 0) return;
+            if(N == 0) break;
 
             map = new int[N][N];
-            dist = new int[N][N];
             v = new boolean[N][N];
 
             for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < N; j++) {
+                for (int j = 0; j < N; j++)
                     map[i][j] = Integer.parseInt(st.nextToken());
-                    dist[i][j] = INF;
-                }
             }
 
-            bfs(0,0);
-
-            System.out.println("Problem " + (cnt++) + ": " + dist[N-1][N-1]);
+            sb.append("Problem " + (cnt++) + ": " + bfs()).append("\n");
         }
+        System.out.println(sb);
     }
 
-    private static void bfs(int a, int b) {
-        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.dist - o2.dist);
-        dist[a][b] = map[0][0];
-        v[a][b] = true;
-        pq.offer(new Node(a, b, map[0][0]));
+    private static int bfs() {
+        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
+        v[0][0] = true;
+        pq.offer(new Node(0, 0, map[0][0]));
 
         while(!pq.isEmpty()){
             Node curr = pq.poll();
 
+            if(curr.r == N-1 && curr.c == N-1){
+                return curr.cost;
+            }
+
             for(int d = 0 ; d < 4 ; d ++){
                 int nr = dr[d] + curr.r;
                 int nc = dc[d] + curr.c;
-                if(nr >= 0 && nr < N && nc >= 0 && nc < N){
-                    if(!v[nr][nc] && dist[nr][nc] > curr.dist + map[nr][nc] ){
-                        dist[nr][nc] = dist[curr.r][curr.c] + map[nr][nc];
-                        v[nr][nc] = true;
-                        pq.offer(new Node(nr, nc, dist[nr][nc]));
-                    }
-                }
+
+                if(!isValid(nr, nc) || v[nr][nc]) continue;
+                v[nr][nc] = true;
+
+                pq.offer(new Node(nr, nc, curr.cost + map[nr][nc]));
             }
         }
+        return 0;
+    }
+
+    private static boolean isValid(int nr, int nc) {
+        return (nr >= 0 && nr < N && nc >= 0 && nc < N);
     }
 }
