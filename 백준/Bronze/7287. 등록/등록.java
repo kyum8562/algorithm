2f4d19b2997896ae41e8d8c496de55b1503045d8
giraffe_kim <kyum8562@gmail.com>
2024-01-07 13:43:04 +0900

    [Bronze V] Title: 등록, Time: 116 ms, Memory: 13832 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println(632);
+        System.out.println("kyum8562");
+       
+    }
+}
