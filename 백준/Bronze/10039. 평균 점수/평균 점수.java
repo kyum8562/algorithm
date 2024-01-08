de7eea6533a033b6c2b084008215c80316aaf651
giraffe_kim <kyum8562@gmail.com>
2024-01-08 14:15:56 +0900

    [Bronze IV] Title: 평균 점수, Time: 124 ms, Memory: 14052 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,18 @@
+import java.io.*;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int sum = 0 ;
+		for(int i = 0 ; i < 5 ; i ++) {
+			int val = Integer.parseInt(br.readLine());
+			
+			sum += val > 40 ? val : 40;
+		}
+		System.out.println(sum/5);
+	}
+ 
+}
