e952cfa92dfa9301f21df9fb1343c3ccda5f8a77
giraffe_kim <kyum8562@gmail.com>
2024-01-10 10:07:46 +0900

    [Silver II] Title: A → B, Time: 144 ms, Memory: 16048 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -23,7 +23,7 @@ public class Main {
             for(int d = 0 ; d < 2 ; d ++){
                 long next = curr[0];
                 if(d == 0) next *= 2;
-                else next = Long.parseLong(next+""+1);
+                else next = next*10+1;
 
                 if(next > e) continue;
                 q.offer(new int[]{(int)next, curr[1]+1});
