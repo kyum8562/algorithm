218065d0b16692cdf569483d4e3c696d5823c12d
giraffe_kim <kyum8562@gmail.com>
2024-01-12 16:27:15 +0900

    [Gold V] Title: 실질적 약수, Time: 1896 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Gold/2247. 실질적 약수/실질적 약수.java
+++ 백준/Gold/2247. 실질적 약수/실질적 약수.java
@@ -1,18 +1,15 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         long K = Long.parseLong(br.readLine());
         long ans = 0;
-        long sum = 0;
 
-        for(long i = 1 ; i <= K ; i ++) {
+        for(long i = 2 ; i < K ; i ++) {
             if(K/i*i - i == 0) break;
-            sum += K/i*i - i;
+            ans += K/i*i - i; // i의 배수가 몇개 있는지 확인
         }
-        ans = sum + 1 - K;
         System.out.println(ans%1000000);
     }
 }
