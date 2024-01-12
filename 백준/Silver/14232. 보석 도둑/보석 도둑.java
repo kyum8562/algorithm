35aed88922d415f181debeeff6d002f768de1754
giraffe_kim <kyum8562@gmail.com>
2024-01-12 14:12:36 +0900

    [Silver I] Title: 보석 도둑, Time: 144 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/14232. 보석 도둑/보석 도둑.java
+++ 백준/Silver/14232. 보석 도둑/보석 도둑.java
@@ -1,35 +1,22 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-// 소인수 분해
-// 리스트에 담아서 출력하려고 했는데 시간초과 고려해 sb에 넣고 출력
-public static void main(String[] args) throws NumberFormatException, IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    long K0 = Long.parseLong(br.readLine());
-    StringBuilder sb = new StringBuilder();
-    
-    long cnt = 0,K=K0;
-    
-    // int로 돌고 long으로 형변환 X -> long으로 for 루프
-    for(long i = 2 ; i*i <= K0 && K>1 ; i ++) {
-        while(K%i == 0) {
-            cnt++;
-            sb.append(i);
-            sb.append(" ");
-            K/=i;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long N = Long.parseLong(br.readLine());
+        long spare = N;
+        List<Long> list = new ArrayList<>();
+        for(long i = 2 ; i * i <= N ; i ++){
+            while(spare % i == 0){
+                list.add(i);
+                spare /= i;
+            }
         }
-    }
-    if(K>1) {
-        cnt++;
-        sb.append(K);
-    }
-    // 출력
-//        sb.insert(0,  cnt + "\n");
-        System.out.println(cnt);
-        System.out.println(sb);
-    }
+        if(spare > 1) list.add(spare);
 
+        System.out.println(list.size());
+        for(Long i: list)
+            System.out.print(i + " ");
+    }
 }
