6eda31eeea4ca5178caa67807c9dd196f8c1550d
giraffe_kim <kyum8562@gmail.com>
2024-01-21 16:27:49 +0900

    [Silver V] Title: 사과 담기 게임, Time: 124 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.java
+++ 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.java
@@ -3,32 +3,30 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        //입력값 처리하는 BufferedReader
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //결과값 출력하는 BufferedWriter
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int j = Integer.parseInt(br.readLine());
-        int answer = 0;	//이동 횟수 저장 변수
-        int start = 1, end = M;	//바구니 시작, 끝의 위치
-        //사과 바구니에 담기 탐색!
-        for(int i=0;i<j;i++){
-            int apple = Integer.parseInt(br.readLine());
-            if(start > apple){		//시작 위치 > 사과 위치
-                answer += start - apple;	//이동한 거리 더하기
-                end -= start - apple;	//끝 위치 변경
-                start = apple;		//시작 위치 변경
-            }else if(end < apple){	//끝 위치 < 사과 위치
-                answer += apple - end;	//이동한 거리 더하기
-                start += apple - end;	//시작 위치 변경
-                end = apple;	//끝 위치 변경
+        int J = Integer.parseInt(br.readLine());
+
+        int curS = 1;
+        int curE = curS + M - 1;
+        int ans = 0;
+        for(int i = 0 ; i < J ; i ++){
+            int tgt = Integer.parseInt(br.readLine());
+
+            if(tgt > curE){
+                ans += tgt - curE;
+                curE = tgt;
+                curS = curE - M + 1;
+            }
+            else if(tgt < curS){
+                ans += curS - tgt;
+                curS = tgt;
+                curE = curS + M - 1;
             }
         }
-        bw.write(answer + "");		//이동 거리 BufferedWriter 저장
-        bw.flush();		//결과 출력
-        bw.close();
-        br.close();
+
+        System.out.println(ans);
     }
 }
