398bc64f9d82f18f25bf61b4f0350a363e69886c
giraffe_kim <kyum8562@gmail.com>
2024-01-22 10:10:39 +0900

    [Gold IV] Title: 부분합, Time: 280 ms, Memory: 24280 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,37 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static int N, M;
+    static final int INF = Integer.MAX_VALUE;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+//        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        long[] arr = new long[N];
+        int ans = INF, s = 0, e = 0;
+        long sum = 0;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int N = Integer.parseInt(st.nextToken());
-		int S = Integer.parseInt(st.nextToken());
-		long[] arr = new long[N];
-		int ans = 100001, p1 = 0, p2 = 0;
-		long sum = 0;
-		st = new StringTokenizer(br.readLine(), " ");
-		for(int i = 0 ; i < N; i ++) {
-//			if(curr >= S || ans == 1) {
-//				System.out.println(1);
-//				return ;
-//			}
-			arr[i] = Long.parseLong(st.nextToken());
-		}
-		while(true) {
-			if(sum >= S) {
-				int tmp = p2-p1;
-				sum -= arr[p1++];
-				if(ans > tmp) ans = tmp;
-			}
-			else if(p2 == N)
-				break;
-			else
-				sum += arr[p2++];
-		}
-		System.out.println(ans==100001?0:ans);
-	}
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < N; i ++)
+            arr[i] = Long.parseLong(st.nextToken());
+
+        while(true) {
+            if(sum >= S) {
+                int tmp = e-s;
+                sum -= arr[s++];
+                if(ans > tmp) ans = tmp;
+            }
+            else if(N == e) break;
+            else sum += arr[e++];
+        }
+        System.out.println(ans == INF ? 0 : ans);
+    }
 }
