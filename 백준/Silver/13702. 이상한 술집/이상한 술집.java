33513df8d126be1aa64f960f032ef37cf4d216db
giraffe_kim <kyum8562@gmail.com>
2024-02-02 16:47:29 +0900

    [Silver III] Title: 이상한 술집, Time: 180 ms, Memory: 17364 KB -BaekjoonHub

---
+++ 백준/Silver/13702. 이상한 술집/이상한 술집.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int N, M;
+    static long[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new long[N+1];
+        long max = Long.MIN_VALUE;
+
+        for(int i = 1 ; i <= N ; i ++){
+            arr[i] = Integer.parseInt(br.readLine());
+            max = Math.max(max, arr[i]); // max 값 저장
+        }
+
+        System.out.println(binarySearch(max));
+    }
+
+    private static long binarySearch(long max) {
+        long s = 1;
+        long e = max;
+        long m = 0; // 나눌 막걸리량(ml) 선정
+        long ans = 0;
+
+        while(e >= s){
+            m = (s + e) / 2;
+
+            long res = 0;
+
+            for(int i = 1 ; i <= N ; i ++)
+                res += arr[i] / m;
+
+            if(res >= M){
+                s = m + 1;
+                ans = m;
+            }
+            else e = m - 1;
+        }
+        return ans;
+    }
+}
