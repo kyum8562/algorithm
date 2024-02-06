96cd24f6cebc39e2b195d9fcf44e054981068537
giraffe_kim <kyum8562@gmail.com>
2024-02-06 12:31:38 +0900

    [Gold V] Title: 용액, Time: 376 ms, Memory: 28080 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -1,37 +1,53 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static int N, ans, lowN, highN, flag;
+    static int[] map;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		
-		long[] arr = new long[n];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=0; i<n; i++) {
-			arr[i] = Long.parseLong(st.nextToken());
-		}
-		
-		int left =0;
-		int right =n-1;
-		int ml =0, mr = 0;
-		long min = Long.MAX_VALUE;
-		while(left<right) {
-			long sum = arr[left]+arr[right];
-			if(min > Math.abs(sum)) {
-				min = Math.abs(sum);
-				ml = left; mr = right;
-			}
-			if(sum>=0) {
-				right--;	
-			}else {
-				left++;
-			}
-		}
-		System.out.println(arr[ml] +" "+arr[mr]);
-	}
+        N = Integer.parseInt(br.readLine());
+
+        map = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < N ; i ++)
+            map[i] = Integer.parseInt(st.nextToken());
+
+        ans = Integer.MAX_VALUE; // 절댓값의 최솟값을 저장하는 변수
+        flag = 1; // 순회를 멈출지 결정하는 상태값(0: 그만 동작해도 되는 상태)
+
+        for (int i = 0; i < N-1; i++){
+            binarySearch(i, map[i]);
+            if(flag == 0) break;
+        }
+
+        System.out.println(lowN + " " + highN);
+    }
+
+    private static void binarySearch(int idx, int A) {
+        int s = idx+1;
+        int e = N-1;
+        int m = 0;
+
+        while(e >= s){
+            m = (s + e) / 2;
+            int res = Math.abs(map[m] + A);
+
+            // 절댓값이 더 작다면, 갱신
+            if(ans >= res){
+                ans = res;
+                lowN = map[idx];
+                highN = map[m];
+            }
+
+            // 두 값의 합이 0이라면, 더 이상 탐색할 필요없음
+            if(map[m] + map[idx] == 0) break;
+            // 두 값의 합이 0보다 크다면, e값을 줄여줌
+            if(map[m] + map[idx] > 0) e = m - 1;
+            // 두 값의 합이 0보다 작다면, s값을 늘려줌
+            else s = m + 1;
+        }
+    }
 }
