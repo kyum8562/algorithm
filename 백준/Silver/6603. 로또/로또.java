d4d2498f26f6f355b07e30e3d842ad4e3ca0b5e6
giraffe_kim <kyum8562@gmail.com>
2024-02-16 19:06:29 +0900

    [Silver II] Title: 로또, Time: 128 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -1,26 +1,30 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-    static int N, ans = 0;
+    static int N;
     static int[] map, choice;
     static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws  IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        while(true){
+
+        while (true){
             st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
+
             if(N == 0) break;
 
-            choice = new int[6];
             map = new int[N];
-            for(int i = 0 ; i < N ; i ++){
+            choice = new int[6];
+
+            for(int i = 0 ; i < N ; i ++)
                 map[i] = Integer.parseInt(st.nextToken());
-            }
 
             comb(0, 0);
             sb.append("\n");
         }
+
         System.out.println(sb);
     }
 
@@ -32,7 +36,7 @@ public class Main {
             return;
         }
 
-        for(int i = start ; i < N ; i ++){
+        for (int i = start; i < N; i++) {
             choice[depth] = map[i];
             comb(depth+1, i+1);
         }
