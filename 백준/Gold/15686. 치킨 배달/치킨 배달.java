0760944e0ae29c2923405ad64c522092f7b2e946
giraffe_kim <kyum8562@gmail.com>
2024-02-17 13:57:35 +0900

    [Gold V] Title: 치킨 배달, Time: 204 ms, Memory: 15792 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,69 +1,80 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-	static int N, M, ans = Integer.MAX_VALUE;
-	static int[][] map;
-	static List<Node> houseList; // 집의 좌표가 담겨있는 list
-	static List<Node> chickenList; // 치킨집의 좌표가 담겨있는 list
-	static int[] choice; // M개를 뽑은 치킨집 인덱스가 담긴 배열 
-	static class Node{ // 좌표(r, c)
-		int r;
-		int c;
-		public Node(int r, int c) {
-			this.r = r;
-			this.c = c;
-		}
-	}
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		houseList = new ArrayList<>();
-		chickenList = new ArrayList<>();
-		choice = new int[M];
-		map = new int[N][N];
-		for(int i = 0 ; i < N ; i ++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j = 0 ; j < N ; j ++) {
-				int v = Integer.parseInt(st.nextToken());
-				map[i][j] = v;
-				if(v == 1) { // 집
-					houseList.add(new Node(i, j));
-				}
-				else if(v == 2) { // 치킨집
-					chickenList.add(new Node(i, j));
-				}
-			}
-		}
-		
-		comb(0, 0); // M개의 치킨집 뽑기
-		System.out.println(ans); // 출력
-	}
-	private static void comb(int depth, int start) {
-		if(depth == M) {
-			ans = Math.min(ans, counting()); // ans = 모든 집에 대한 최소의 치킨거리 갱신
-			return;
-		}
-		
-		for(int i = start ; i < chickenList.size(); i ++) {
-			choice[depth] = i;
-			comb(depth+1, i+1);
-		}
-	}
-	private static int counting() {
-		int sum = 0; // 모든 집의 최소거리의 합이 담기는 변수
-		for(int j = 0 ; j < houseList.size(); j ++) { // 집만큼 순회
-			int minVal = Integer.MAX_VALUE;
-			for(int i = 0 ; i < M ; i ++) { // 뽑은 치킨집 순회
-				int curChickenR = chickenList.get(choice[i]).r;
-				int curChickenC = chickenList.get(choice[i]).c;
-				// 맨해튼 거리 구하기
-				int res = Math.abs(curChickenR - houseList.get(j).r) + Math.abs(curChickenC - houseList.get(j).c);
-				minVal = Math.min(minVal, res); // 최소 거리의 치킨집 탐색
-			}
-			sum += minVal; // 집-치킨집 간의 최소거리 sum
-		}
-		return sum;
-	}
+    static int N, M, min, chickenHouseLen, houseLen;
+    static int[] choice, countArr;
+    static int[][] map;
+    static List<int[]> chickenHouse, house;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][N];
+        choice = new int[M];
+        min = Integer.MAX_VALUE;
+
+        chickenHouse = new ArrayList<>();
+        house = new ArrayList<>();
+
+        for(int i = 0 ; i < N ; i ++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0 ; j < N ; j ++){
+                int tmp = Integer.parseInt(st.nextToken());
+                map[i][j] = tmp;
+
+                if(tmp == 1)
+                    house.add(new int[] {i, j});
+                else if(tmp == 2)
+                    chickenHouse.add(new int[] {i, j});
+            }
+        }
+
+        chickenHouseLen = chickenHouse.size();
+        houseLen = house.size();
+
+        // 치킨 집 찾기
+        perm(0, 0);
+
+        System.out.println(min);
+    }
+
+    private static void perm(int depth, int start) {
+        if (depth == M) {
+            countArr = new int[houseLen];
+            Arrays.fill(countArr, Integer.MAX_VALUE);
+
+            play();
+            min = Math. min(min, counting());
+            return;
+        }
+        for (int i = start; i < chickenHouseLen; i++) {
+            choice[depth] = i;
+            perm(depth+1, i+1);
+        }
+    }
+
+    private static int counting() {
+        int sum = 0;
+        for(int v: countArr)
+            sum += v;
+        return sum;
+    }
+
+    private static void play() {
+        for(int i = 0 ; i < M ; i ++){
+            int cur = choice[i];
+            int r = chickenHouse.get(cur)[0];
+            int c = chickenHouse.get(cur)[1];
+
+            for(int j = 0 ; j < houseLen ; j ++){
+                int dist = Math.abs(house.get(j)[0] - r) + Math.abs(house.get(j)[1] - c);
+                countArr[j] = Math.min(countArr[j], dist);
+            }
+        }
+    }
 }
