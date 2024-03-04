e17420523ac2f6d41c7c2679d162763e30d1b596
giraffe_kim <kyum8562@gmail.com>
2024-03-05 00:40:50 +0900

    [Gold I] Title: 구간 곱 구하기, Time: 536 ms, Memory: 109236 KB -BaekjoonHub

--- 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.java
+++ 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.java
@@ -1,67 +1,64 @@
 import java.io.*;
 import java.util.*;
-class Main {
-    static final int DIVISION_NUM = 1_000_000_007;
-    static int N, Q;
-    static long[] map, tree;
-    public static void main(String[] args) throws Exception {
-//        System.setIn(new FileInputStream("src/input.txt"));
 
+class Main {
+    static final int MAXVAL = 1_000_000_007;
+    static long[] map, tree;
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
         st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        Q = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
 
         map = new long[N+1];
         tree = new long[N*4];
 
-        for (int i = 1; i <= N; i++)
+        for(int i = 1 ; i <= N ; i ++)
             map[i] = Long.parseLong(br.readLine());
 
-        // 세그먼트 트리 생성
         init(1, N, 1);
 
-        for(int i = 0 ; i < Q ; i ++){
+        for(int i = 0 ; i < K ; i ++){
             st = new StringTokenizer(br.readLine());
             int type = Integer.parseInt(st.nextToken());
             int A = Integer.parseInt(st.nextToken());
             long B = Long.parseLong(st.nextToken());
 
-            // 수열 업데이트
+            // 값 업데이트
             if(type == 1){
                 map[A] = B;
                 update(1, N, 1, A, B);
             }
             // 구간 곱 구하기
             else{
-                sb.append(multiple(1, N, 1, A, (int)B)).append("\n");
+                sb.append(multiple(1, N, 1, A, B)).append("\n");
             }
         }
-        System.out.println(sb);
+        System.out.print(sb);
     }
     private static long init(int start, int end, int node) {
-        if(start == end) return tree[node] = map[start];
+        if(start == end)
+            return tree[node] = map[start];
 
-        int mid = (start+end) / 2;
-        return tree[node] = init(start, mid, node*2) * init(mid+1, end, node*2+1) % DIVISION_NUM;
+        int mid = (start + end) / 2;
+        return tree[node] = init(start, mid, node*2) * init(mid+1, end, node*2+1) % MAXVAL;
     }
-    private static long update(int start, int end, int node, int tgtIdx, long newValue) {
-        if(tgtIdx > end || tgtIdx < start) return tree[node];
+    private static long update(int start, int end, int node, int tgtIdx, long newVal) {
+        if(tgtIdx > end || start > tgtIdx) return tree[node];
 
-        if(start == end) return tree[node] = newValue;
+        if(start == end) return tree[node] = newVal;
 
-        int mid = (start+end) / 2;
-        return tree[node] = update(start, mid, node*2, tgtIdx, newValue) * update(mid+1, end, node*2+1, tgtIdx, newValue) % DIVISION_NUM;
+        int mid = (start + end) / 2 ;
+        return tree[node] = update(start, mid, node*2, tgtIdx, newVal) * update(mid+1, end, node*2+1, tgtIdx, newVal) % MAXVAL;
     }
-
-    private static long multiple(int start, int end, int node, int left, int right) {
+    private static long multiple(int start, int end, int node, int left, long right) {
         if(left > end || right < start) return 1;
         if(start >= left && right >= end) return tree[node];
 
-        int mid = (start+end) / 2;
-        return (multiple(start, mid, node*2, left, right) * multiple(mid+1, end, node*2+1, left, right)) % DIVISION_NUM;
+        int mid = (start + end) / 2;
+        return multiple(start, mid, node*2, left, right) * multiple(mid+1, end, node*2+1, left, right) % MAXVAL;
     }
 }
