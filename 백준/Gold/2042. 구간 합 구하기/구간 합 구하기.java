0087659c1d9971220c690e02ae79b4529d6b60f9
giraffe_kim <kyum8562@gmail.com>
2024-03-04 15:41:14 +0900

    [Gold I] Title: 구간 합 구하기, Time: 592 ms, Memory: 116392 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -1,77 +1,70 @@
 import java.io.*;
 import java.util.*;
-
-public class Main {
-    static int N;
+class Main {
+    static int N, Q;
     static long[] map, tree;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+//        System.setIn(new FileInputStream("src/input.txt"));
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
-        int Q = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
+        Q = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
 
-        map = new long[N + 1];
+        map = new long[N+1];
         tree = new long[N*4];
 
         for (int i = 1; i <= N; i++)
             map[i] = Long.parseLong(br.readLine());
 
-        setSegmentTree(1, N, 1);
+        // 세그먼트 트리 생성
+        init(1, N, 1);
 
-        for (int i = 0; i < Q; i++) {
+        for(int i = 0 ; i < Q ; i ++){
             st = new StringTokenizer(br.readLine());
             int type = Integer.parseInt(st.nextToken());
-            int a = Integer.parseInt(st.nextToken());
-            long b = Long.parseLong(st.nextToken());
+            int A = Integer.parseInt(st.nextToken());
+            long B = Long.parseLong(st.nextToken());
 
-            // 특정 값 update
+            // 수열 업데이트
             if(type == 1){
-                long newVal = b - map[a];
-                map[a] = b;
-                setUpdate(1, N, 1, a, newVal);
+                long newValue = B - map[A];
+                map[A] = B;
+                update(1, N, 1, A, newValue);
             }
             // 구간 합 구하기
-            else
-                sb.append(getSum(1, N, 1, a, (int) b)).append("\n");
-
+            else{
+                sb.append(sum(1, N, 1, A, (int)B)).append("\n");
+            }
         }
         System.out.println(sb);
     }
+    private static long init(int start, int end, int node) {
+        if(start == end) return tree[node] = map[start];
 
-    private static long getSum(int start, int end, int node, int left, int right) {
-        // 범위 밖에 있을 경우
-        if(left > end || right < start) return 0;
-
-        // 범위 안에 있을 경우
-        if(start >= left && right >= end)
-            return tree[node];
-
-        int mid = (start + end) / 2;
-        return getSum(start, mid, node*2, left, right) + getSum(mid+1, end, node*2+1, left, right);
+        int mid = (start+end) / 2;
+        return tree[node] = init(start, mid, node*2) + init(mid+1, end, node*2+1);
     }
+    private static void update(int start, int end, int node, int tgtIdx, long newValue) {
+        if(tgtIdx > end || tgtIdx < start) return;
 
-    private static void setUpdate(int start, int end, int node, int targetIdx, long newVal) {
-        // 범위 밖에 있을 경우
-        if(targetIdx > end || targetIdx < start) return;
-
-        // 범위 안에 있으면 아래로 내려가며 다른 노드도 갱신
-        tree[node] += newVal;
+        tree[node] += newValue;
 
         if(start == end) return;
 
-        int mid = (start + end) / 2;
-        setUpdate(start, mid, node*2, targetIdx, newVal);
-        setUpdate(mid+1, end, node*2+1, targetIdx, newVal);
+        int mid = (start+end) / 2;
+        update(start, mid, node*2, tgtIdx, newValue);
+        update(mid+1, end, node*2+1, tgtIdx, newValue);
     }
 
-    private static long setSegmentTree(int start, int end, int node) {
-        if(start == end)
-            return tree[node] = map[start];
+    private static long sum(int start, int end, int node, int left, int right) {
+        if(left > end || right < start) return 0;
+        if(start >= left && right >= end) return tree[node];
 
-        int mid = (start + end) / 2;
-        return tree[node] = setSegmentTree(start, mid, node*2) + setSegmentTree(mid+1, end, node*2+1);
+        int mid = (start+end) / 2;
+        return sum(start, mid, node*2, left, right) + sum(mid+1, end, node*2+1, left, right);
     }
 }
