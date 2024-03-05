a5ad40903fc5c620858406eadf15d93018e98269
giraffe_kim <kyum8562@gmail.com>
2024-03-05 15:20:27 +0900

    [Gold IV] Title: 전화번호 목록, Time: 584 ms, Memory: 216132 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -12,12 +12,10 @@ class Main {
         for (int t = 0; t < T; t++) {
             root = new TrieNode();
             int N = Integer.parseInt(br.readLine());
-            String[] strs = new String[N];
             boolean flag = true;
 
             for(int i = 0 ; i < N ; i ++){
-                strs[i] = br.readLine();
-                if(!insert(strs[i]))
+                if(!insert(br.readLine()))
                     flag = false;
             }
 
