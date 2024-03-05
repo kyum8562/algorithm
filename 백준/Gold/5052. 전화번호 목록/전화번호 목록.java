1453e8aad94551369f23f3b8fbcdd7f3ec6e79fd
giraffe_kim <kyum8562@gmail.com>
2024-03-05 17:15:54 +0900

    [Gold IV] Title: 전화번호 목록, Time: 560 ms, Memory: 216560 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -8,17 +8,18 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int T = Integer.parseInt(br.readLine());
-
-        for (int t = 0; t < T; t++) {
-            root = new TrieNode();
+        for(int t = 1 ; t <= T ; t ++){
             int N = Integer.parseInt(br.readLine());
             boolean flag = true;
 
-            for(int i = 0 ; i < N ; i ++){
-                if(!insert(br.readLine()))
-                    flag = false;
-            }
+            root = new TrieNode();
 
+            for(int i = 0 ; i < N ; i ++){
+                // 삽입과 접두어 판단여부
+                if(!insert(br.readLine())){
+                    flag = false;
+                }
+            }
             sb.append(flag ? "YES" : "NO").append("\n");
         }
         System.out.print(sb);
@@ -28,24 +29,25 @@ class Main {
         TrieNode cur = root;
         int len = str.length();
 
-        for(int j = 0 ; j < len ; j ++){
-            char c = str.charAt(j);
-            if(cur.childNode.get(c) == null)
-                cur.childNode.put(c, new TrieNode());
+        for(int i = 0 ; i < len ; i ++){
+            char c = str.charAt(i);
 
-            cur = cur.childNode.get(str.charAt(j));
+            if(cur.children.get(c) == null)
+                cur.children.put(c, new TrieNode());
+
+            cur = cur.children.get(c);
 
             if(cur.isLeaf) return false;
         }
 
-        if(cur.childNode.size() != 0) return false;
+        if(cur.children.size() != 0) return false;
 
         cur.isLeaf = true;
         return true;
     }
 
     static class TrieNode{
+        Map<Character, TrieNode> children = new LinkedHashMap<>();
         boolean isLeaf;
-        Map<Character, TrieNode> childNode = new LinkedHashMap<>();
     }
 }
