f8b78d6848625f5173aee59adad5cc1f3ab1e0bb
giraffe_kim <kyum8562@gmail.com>
2024-03-08 16:43:32 +0900

    [Gold IV] Title: 공룡게임, Time: 128 ms, Memory: 15184 KB -BaekjoonHub

--- 백준/Gold/20544. 공룡게임/공룡게임.java
+++ 백준/Gold/20544. 공룡게임/공룡게임.java
@@ -16,15 +16,7 @@ class Main {
 
         ans = 0;
         map = new int[N];
-        dp = new int[N][4][3][3];
-
-        for(int i = 0 ; i < N ; i ++){
-            for(int j = 0 ; j < 4 ; j ++){
-                for(int k = 0 ; k < 3 ; k ++){
-                    Arrays.fill(dp[i][j][k], -1);
-                }
-            }
-        }
+        dp = new int[N][3 + 1][2 + 1][2];
 
         System.out.println(dfs(1, 0, 0, 0));
     }
@@ -54,26 +46,18 @@ class Main {
 //        dfs(depth+1, 0, 0, isTwoHeight);
 //    }
     // [3. 탑다운 DP]
-
-    /**
-     * @param depth: 현재 위치
-     * @param prevHeight2Cnt: 이전까지 연속해서 넘은 높이가 2인 장애물 수
-     * @param cactusCnt: 이전까지 연속해서 넘은 선인장 수
-     * @param isTwoHeight: 높이가 2인 선인장을 넘었는지 여부
-     * @return N의 위치까지 도달한 수
-     */
-    private static long dfs(int depth, int prevHeight2Cnt, int cactusCnt, int isTwoHeight) {
-        if (prevHeight2Cnt == 2 || cactusCnt > 2) return 0;
+    private static long dfs(int depth, int curHeight, int cactusCnt, int isTwoHeight) {
+        if (curHeight >= 4 || cactusCnt > 2) return 0;
         if (depth == N) return isTwoHeight;
 
-        if (dp[depth][prevHeight2Cnt][cactusCnt][isTwoHeight] != -1)
-            return dp[depth][prevHeight2Cnt][cactusCnt][isTwoHeight];
+        if (dp[depth][curHeight][cactusCnt][isTwoHeight] != 0)
+            return dp[depth][curHeight][cactusCnt][isTwoHeight];
 
-        return dp[depth][prevHeight2Cnt][cactusCnt][isTwoHeight]
-                = (int)((
-                        dfs(depth + 1, prevHeight2Cnt + 1, cactusCnt + 1, 1) +
-                        dfs(depth + 1, 0, cactusCnt + 1, isTwoHeight) +
-                        dfs(depth + 1, 0, 0, isTwoHeight)
-                        )% DIVIDE_NUM);
+        return dp[depth][curHeight][cactusCnt][isTwoHeight]
+                = (int) ((
+                            dfs(depth + 1, curHeight + 2, cactusCnt + 1, 1) +
+                            dfs(depth + 1, curHeight + 1, cactusCnt + 1, isTwoHeight) +
+                            dfs(depth + 1, 0, 0, isTwoHeight)
+                        ) % DIVIDE_NUM);
     }
 }
