f4fcc784c561c30714548f231df308847c3df964
giraffe_kim <kyum8562@gmail.com>
2024-03-21 01:24:41 +0900

    [Bronze II] Title: 거스름돈, Time: 120 ms, Memory: 14076 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.java
+++ 백준/Bronze/5585. 거스름돈/거스름돈.java
@@ -1,22 +1,24 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = 1000 - Integer.parseInt(br.readLine());
-        int[] arr = {500, 100, 50, 10, 5, 1};
-        int arrCnt = 0;
-        int cnt = 0;
 
-        while(N != 0){
-            if(N >= arr[arrCnt]){
-                cnt += N / arr[arrCnt];
-                N %= arr[arrCnt];
+        int N = 1000 - Integer.parseInt(br.readLine());
+        int cnt = 0;
+        int ans = 0;
+        int[] arr = {500, 100, 50, 10, 5, 1};
+
+        while(cnt < 6){
+            if(N >= arr[cnt]) {
+                ans += N / arr[cnt];
+                N %= arr[cnt];
             }
-            arrCnt++;
+
+            cnt ++;
         }
 
-        System.out.println(cnt);
+        System.out.print(ans);
     }
 }
