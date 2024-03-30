c99bd17f8adbdee73515eb975b8a2b1e27f2efb3
giraffe_kim <kyum8562@gmail.com>
2024-03-31 02:45:21 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 144 ms, Memory: 14688 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -1,78 +1,53 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main {
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	static int N, M, ans = Integer.MAX_VALUE, maxCnt = Integer.MAX_VALUE;
-	static List<ArrayList<Integer>> list;
-	static boolean[] isVisited;
-	static Queue<Integer> q = new ArrayDeque<>();
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = stoi(st.nextToken());
-		M = stoi(st.nextToken());
-		list = new ArrayList<>();
-		
-		for(int i = 0 ; i <= N ; i ++) {
-			list.add(new ArrayList<>());
-		}
-		
-		for(int i = 0 ; i < M ; i ++) {
-			st = new StringTokenizer(br.readLine());
-			int a = stoi(st.nextToken());
-			int b = stoi(st.nextToken());
-			
-			list.get(a).add(b);
-			list.get(b).add(a);
-		}
-		
-		for(int i = 1 ; i <= N ; i ++) {
-			isVisited = new boolean[N+1];
-			int cnt = bfs(i);
-			if(maxCnt > cnt) {
-				maxCnt = cnt;
-				ans = i;
-			}
-			else if(maxCnt == cnt && ans > i) {
-				ans = i;
-			}
-		}
-		
-		System.out.println(ans);
-	}
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-	private static int bfs(int start) {
-		q.offer(start);
-		isVisited[start] = true;
-		int cnt = 0;
-		int depth = 1;
-		while(!q.isEmpty()) {
-			int qSize = q.size();
-			for(int j = 0 ; j < qSize ; j ++) {
-				int curr = q.poll();
-				for(int i = 0 ; i < list.get(curr).size() ; i ++) {
-					int tmp = list.get(curr).get(i);
-					if(!isVisited[tmp]) {
-						q.offer(tmp);
-						isVisited[tmp] = true;
-						cnt += depth;
-					}
-				}
-			}
-			depth++;
-		}
-		return cnt;
-	}
+        int[][] dist = new int[N+1][N+1];
+        for(int i = 1 ; i <= N ; i ++)
+            Arrays.fill(dist[i], 1 << 20);
 
-	static int stoi(String s) {
-		return Integer.parseInt(s);
-	}
+        for(int i = 0 ; i < M ; i ++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            dist[a][b] = 1;
+            dist[b][a] = 1;
+        }
+
+        for(int i = 1 ; i <= N ; i ++){
+            dist[i][i] = 0;
+
+            for(int j = 1 ; j <= N ; j ++){
+                for(int k = 1 ; k <= N ; k ++){
+                    if(dist[j][k] > dist[j][i] + dist[i][k]){
+                        dist[j][k] = dist[j][i] + dist[i][k];
+                    }
+                }
+            }
+        }
+
+        int min = 1 << 20;
+        int ansIdx = -1;
+        for(int i = N ; i >= 1 ; i --){
+            int bakenCnt = 0;
+            for(int j = 1 ; j <= N ; j ++)
+                bakenCnt += dist[i][j];
+
+            if(min > bakenCnt){
+                min = bakenCnt;
+                ansIdx = i;
+            }
+            else if(min == bakenCnt) ansIdx = i;
+        }
+
+        System.out.println(ansIdx);
+    }
 }
