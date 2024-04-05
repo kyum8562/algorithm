04154e4500dfa1ed0ce91759b6c721e1cd5b864e
giraffe_kim <kyum8562@gmail.com>
2024-04-05 20:28:26 +0900

    [Gold IV] Title: 트리의 지름, Time: 232 ms, Memory: 23376 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -2,56 +2,62 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, ans;
-    static boolean[] v;
+    static int N, max, idx;
+    static Integer[] arr;
+    static long[] size;
+    static List<Long> ans;
     static List<Node>[] list;
-
-    static class Node {
-        int n, d;
-        public Node(int n, int d) {
-            this.n = n;
-            this.d = d;
-        }
-    }
-
-    static StringBuilder sb = new StringBuilder();
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
-        list = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++) list[i] = new ArrayList<>();
+        N = Integer.parseInt(br.readLine());
+        max = -1;
 
-        boolean[] leafNodes = new boolean[N + 1];
-        for (int i = 0; i < N - 1; i++) {
+        size = new long[N+1];
+        arr = new Integer[N-1];
+        ans = new ArrayList<>();
+        list = new ArrayList[N+1];
+
+        for(int i = 1 ; i <= N ; i ++){
+            list[i] = new ArrayList<>();
+        }
+
+        for(int i = 0 ; i < N-1 ; i ++){
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
 
-            list[a].add(new Node(b, c));
-            list[b].add(new Node(a, c));
-            leafNodes[a] = true;
+            list[a].add(new Node(b, d));
+            list[b].add(new Node(a, d));
         }
 
-        for (int i = 1; i <= N; i++) {
-            if(leafNodes[i]) continue;
-            v = new boolean[N + 1];
-            v[i] = true;
-            dfs(i, 0);
-        }
+        dfs(1, 0, 0);
+        max = -1;
+        dfs(idx, 0, 0);
 
-        System.out.println(ans);
+        System.out.println(max);
     }
 
-    private static void dfs(int i, int sum) {
-        for (Node next : list[i]) {
-            if(v[next.n]) continue;
-            v[next.n] = true;
-            dfs(next.n, sum+next.d);
+    private static void dfs(int cur, int prev, int curDist) {
+
+        if(curDist > max){
+            max = curDist;
+            idx = cur;
         }
-        ans = Math.max(ans, sum);
+
+        for(Node next: list[cur]){
+            if(next.n == prev) continue;
+            dfs(next.n, cur, curDist + next.d);
+        }
+    }
+    static class Node{
+        int n, d;
+        public Node(int n, int d){
+            this.n = n;
+            this.d = d;
+        }
     }
 }
