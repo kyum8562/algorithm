173e8390e88026fe3ad4cfc75e37df4824875518
giraffe_kim <kyum8562@gmail.com>
2024-04-30 20:13:57 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 768 ms, Memory: 127576 KB -BaekjoonHub

--- 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.java
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -10,25 +10,35 @@ public class Main {
         int P = Integer.parseInt(st.nextToken());
         int ans = 0;
 
-        Stack<Integer>[] stack = new Stack[7];
-        for(int i = 1 ; i <= 6 ; i ++)
+        Stack<Integer>[] stack = new Stack[P + 1];
+        for (int i = 1; i <= P; i++)
             stack[i] = new Stack<>();
 
-        for(int i = 0 ; i < N ; i ++){
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            // 스택이 비어있지 않으며 stack의 마지막 값이 b보다 크다면
-            while(!stack[a].isEmpty() && stack[a].peek() > b){
-                stack[a].pop();
-                ans++;
-            }
-
-            // 비어있거나 b가 stack의 마지막 값보다 크다면
-            if(stack[a].isEmpty() || b > stack[a].peek()){
+            // 해당 스택이 비어있을 경우
+            if (stack[a].isEmpty()) {
                 stack[a].push(b);
                 ans++;
+            }
+            // 해당 스택이 비어있지 않을경우
+            else {
+                // b보다 작거나 같을 때까지 빼줌
+                while (!stack[a].isEmpty()) {
+                    if (stack[a].peek() > b) {
+                        ans ++;
+                        stack[a].pop();
+                    } else break;
+                }
+
+                // 해당 스택에서 b를 찾지 못했을 때
+                if(stack[a].isEmpty() || stack[a].peek() != b){
+                    stack[a].push(b);
+                    ans ++;
+                }
             }
         }
 
