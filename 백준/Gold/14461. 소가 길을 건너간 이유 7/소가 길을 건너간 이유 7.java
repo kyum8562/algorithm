a5bcfbcc505140711eeaba1fbacd5f91039bef6c
giraffe_kim <kyum8562@gmail.com>
2024-05-03 23:28:32 +0900

    [Gold II] Title: 소가 길을 건너간 이유 7, Time: 252 ms, Memory: 20684 KB -BaekjoonHub

--- 백준/Gold/14461. 소가 길을 건너간 이유 7/소가 길을 건너간 이유 7.java
+++ 백준/Gold/14461. 소가 길을 건너간 이유 7/소가 길을 건너간 이유 7.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+class Main {
     static int N, T;
     static int[][] map;
     static int[] dr = {-1, 0, 1, 0};
@@ -23,42 +23,52 @@ public class Main {
     }
 
     private static void bfs() {
-        PriorityQueue<Node> q = new PriorityQueue<>((o1, o2) -> o1.d - o2.d);
+        PriorityQueue<Node> pq = new PriorityQueue<>();
         boolean[][][] v = new boolean[N][N][3];
-        q.offer(new Node(0, 0, 0, 0));
+        pq.offer(new Node(0, 0, 0, 0)); // r, c, d, moveCnt
 
-        while(!q.isEmpty()) {
-            Node curr = q.poll();
+        while(!pq.isEmpty()){
+            Node cur = pq.poll();
 
-            if(v[curr.r][curr.c][curr.move]) continue;
-            v[curr.r][curr.c][curr.move] = true;
+            if(v[cur.r][cur.c][cur.moveCnt]) continue;
+            v[cur.r][cur.c][cur.moveCnt] = true;
 
-            if(curr.r == N-1 && curr.c == N-1){
-                System.out.println(curr.d);
+            if(cur.r == N-1 && cur.c == N-1){
+                System.out.println(cur.d);
                 return;
             }
 
             for(int d = 0 ; d < 4 ; d ++){
-                int nr = curr.r + dr[d];
-                int nc = curr.c + dc[d];
+                int nr = cur.r + dr[d];
+                int nc = cur.c + dc[d];
 
-                if(!isValid(nr, nc) || v[nr][nc][(curr.move+1)%3]) continue;
+                if(!isValid(nr, nc) || v[nr][nc][(cur.moveCnt+1)%3]) continue;
 
-                if(curr.move == 2) q.offer(new Node(nr, nc, 0, curr.d + map[nr][nc] + T));
-                else q.offer(new Node(nr, nc, curr.move + 1, curr.d + T));
+                if(cur.moveCnt == 2)
+                    pq.offer(new Node(nr, nc, cur.d + T + map[nr][nc], 0));
+                else
+                    pq.offer(new Node(nr, nc, cur.d + T, cur.moveCnt+1));
+
             }
         }
     }
     static boolean isValid(int nr, int nc){
-        return(nr>=0 && nr<N && nc>=0 && nc<N);
+        return (nr>=0 && nr<N && nc>=0 && nc<N);
     }
-    static class Node{
-        int r, c, move, d;
-        public Node(int r, int c, int move, int d){
+
+    static class Node implements Comparable<Node>{
+        int r, c, d, moveCnt;
+
+        public Node(int r, int c, int d, int moveCnt){
             this.r = r;
             this.c = c;
-            this.move = move;
             this.d = d;
+            this.moveCnt = moveCnt;
+        }
+
+        @Override
+        public int compareTo(Node o){
+            return this.d - o.d;
         }
     }
 }
