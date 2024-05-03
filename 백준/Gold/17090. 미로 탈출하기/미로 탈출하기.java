f41cd4a8d31c51498630c17089a23ac62aef0eb3
giraffe_kim <kyum8562@gmail.com>
2024-05-03 17:05:17 +0900

    [Gold III] Title: 미로 탈출하기, Time: 268 ms, Memory: 47848 KB -BaekjoonHub

---
+++ 백준/Gold/17090. 미로 탈출하기/미로 탈출하기.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static int N, M;
+    static char[][] arr;
+    static int[][] dp;
+    static boolean[][] v;
+    static int[] dr = {-1, 0, 1, 0};
+    static int[] dc = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new char[N][M];
+        dp = new int[N][M];
+        v = new boolean[N][M];
+
+        for(int i = 0 ; i < N ; i ++) {
+            String s = br.readLine();
+            for (int j = 0; j < M; j++)
+                arr[i][j] = s.charAt(j);
+        }
+
+        int cnt = 0;
+        for(int i = 0 ; i < N ; i ++){
+            for(int j = 0 ; j < M ; j ++){
+
+                // 이미 결정이 되었다면
+                if(dp[i][j] == 1) cnt ++;
+                // 결정되지 않았다면
+                else if(dp[i][j] == 0 && !v[i][j]){
+                    cnt += recur(i, j) == 1 ? 1 : 0;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+
+    private static int recur(int r, int c) {
+        if(!isValid(r, c)) return 1; // 성공
+
+        if(dp[r][c] != 0) return dp[r][c];
+
+        if(v[r][c]) return 2; // 실패
+        else v[r][c] = true;
+
+
+        int d = getDir(arr[r][c]);
+
+        int nr = r + dr[d];
+        int nc = c + dc[d];
+
+        dp[r][c] = recur(nr, nc);
+        v[r][c] = false;
+
+        return dp[r][c];
+    }
+
+    private static int getDir(char c) {
+        if(c == 'U') return 0;
+        else if(c == 'R') return 1;
+        else if(c == 'D') return 2;
+        else return 3;
+    }
+
+    private static boolean isValid(int r, int c) {
+        return (r>=0 && r<N && c>=0 && c<M);
+    }
+}
