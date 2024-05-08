adfb66b59f1449e304a07007dc677a1ae951eba0
giraffe_kim <kyum8562@gmail.com>
2024-05-08 18:17:52 +0900

    [Gold V] Title: 보이저 1호, Time: 252 ms, Memory: 19760 KB -BaekjoonHub

--- 백준/Gold/3987. 보이저 1호/보이저 1호.java
+++ 백준/Gold/3987. 보이저 1호/보이저 1호.java
@@ -7,6 +7,7 @@ public class Main {
     static char[][] arr;
     static int[] dr = {-1, 0, 1, 0};
     static int[] dc = {0, 1, 0, -1};
+    static char[] dirArr = {'U', 'R', 'D', 'L'};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -28,92 +29,69 @@ public class Main {
         int sc = Integer.parseInt(st.nextToken())-1;
 
         int max = 1;
-        int dir = 4;
+        int dir = -1;
 
         for(int i = 0 ; i < 4 ; i ++){
-            int res = bfs(sr, sc, i);
+            int res = play(sr, sc, i);
 
+            // 무한루프 케이스
             if(res == INF){
                 max = INF;
                 dir = i;
                 break;
             }
 
+            // 갱신
             if(res > max){
                 max = res;
                 dir = i;
             }
-            else if(res == max){
-                if(dir > i) dir = i;
-            }
         }
 
-        sb.append(parseResD(dir)).append("\n");
+        sb.append(dirArr[dir]).append("\n");
         sb.append(max == INF ? "Voyager" : max).append("\n");
 
         System.out.print(sb);
     }
 
-    private static int bfs(int sr, int sc, int sd) {
-        Queue<Node> q = new ArrayDeque<>();
+    private static int play(int r, int c, int d) {
+        int time = 1;
 
-        int max = 1;
-        q.offer(new Node(sr, sc, sd, 1));
+        while(true){
 
-        while(!q.isEmpty()){
-            Node cur = q.poll();
+            int nr = r + dr[d];
+            int nc = c + dc[d];
 
-            // 정답 갱신
-            max = Math.max(max, cur.t);
-
-            // 무한루프 케이스 처리
-            if(cur.t >= R*C*2) return INF;
-
-            // 현재 인덱스가 '.'일 때
-            int nr = cur.r + dr[cur.d];
-            int nc = cur.c + dc[cur.d];
-
+            // 유효성 및 블랙홀 여부 체크
             if(!isValid(nr, nc) || arr[nr][nc] == 'C') break;
 
-            q.offer(new Node(nr, nc, arr[nr][nc] == '.' ? cur.d : parsingD(cur.d, arr[nr][nc]), cur.t+1));
+            // 무한루프 케이스 처리
+            if(time >= R*C*2) return INF;
+
+            // r,c 갱신
+            r = nr;
+            c = nc;
+
+            // '/' or '\'일 때
+            if(arr[nr][nc] != '.')
+                d = parsingD(d, arr[nr][nc]);
+
+            time ++;
         }
 
-        return max;
+        return time;
     }
 
     private static int parsingD(int d, char c) {
-        if(c == '\\'){
-            if(d == 0) return 3;
-            else if(d == 1) return 2;
-            else if(d == 2) return 1;
-            else return 0;
-        }
+        if(c == '\\')
+            return 3 - d;
         else{
-            if(d == 0) return 1; // 북으로 향하면 동으로 변경
-            else if(d == 1) return 0;
-            else if(d == 2) return 3;
-            else return 2;
+            if(d >= 0 && d <= 1) return 1 - d;
+            else return 5 - d;
         }
-    }
-
-    private static char parseResD(int c) {
-        if(c == 0) return 'U';
-        else if(c == 1) return 'R';
-        else if(c == 2) return 'D';
-        else return 'L';
     }
 
     private static boolean isValid(int nr, int nc) {
         return (nr>=0 && nr<R && nc>=0 && nc<C);
-    }
-
-    static class Node{
-        int r, c, d, t;
-        public Node(int r, int c, int d, int t){
-            this.r = r;
-            this.c = c;
-            this.d = d;
-            this.t = t;
-        }
     }
 }
