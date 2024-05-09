ff625520f2b508287c3865ba2742b21f42dabe73
giraffe_kim <kyum8562@gmail.com>
2024-05-09 16:59:35 +0900

    [Gold V] Title: 트리와 쿼리, Time: 620 ms, Memory: 77672 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
@@ -2,23 +2,23 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] size;
+    static int N;
+    static int[] dp;
     static List<Integer>[] list;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
         int R = Integer.parseInt(st.nextToken());
         int Q = Integer.parseInt(st.nextToken());
 
-        size = new int[N+1];
+        dp = new int[N+1];
         list = new ArrayList[N+1];
 
-        for(int i = 1 ; i <= N ; i ++){
+        for(int i = 1 ; i <= N ; i ++)
             list[i] = new ArrayList<>();
-        }
 
         for(int i = 0 ; i < N-1 ; i ++){
             st = new StringTokenizer(br.readLine());
@@ -29,23 +29,25 @@ public class Main {
             list[b].add(a);
         }
 
-        dfs(R, 0);
+        recur(R, 0);
 
-        for(int i = 0 ; i < Q ; i ++)
-            sb.append(size[Integer.parseInt(br.readLine())]).append("\n");
+        for(int i = 0 ; i < Q ; i ++){
+            int tgt = Integer.parseInt(br.readLine());
+            sb.append(dp[tgt]).append("\n");
+        }
 
         System.out.print(sb);
     }
 
-    private static void dfs(int cur, int prev) {
-        size[cur] = 1;
+    private static void recur(int cur, int prev) {
+        dp[cur] = 1;
 
         for(int next: list[cur]){
             if(next == prev) continue;
 
-            dfs(next, cur);
+            recur(next, cur);
 
-            size[cur] += size[next];
+            dp[cur] += dp[next];
         }
     }
 }
