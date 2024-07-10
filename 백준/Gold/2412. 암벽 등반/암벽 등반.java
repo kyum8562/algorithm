9fdb438f406f685f30c0768f3c5396eb2c480182
giraffe_kim <kyum8562@gmail.com>
2024-07-10 17:33:26 +0900

    [Gold IV] Title: 암벽 등반, Time: 376 ms, Memory: 30044 KB -BaekjoonHub

--- 백준/Gold/2412. 암벽 등반/암벽 등반.java
+++ 백준/Gold/2412. 암벽 등반/암벽 등반.java
@@ -1,17 +1,8 @@
 import java.io.*;
 import java.util.*;
 
-/**
- * 맵을 매번 순회하면서(50,000) 가기.. ? x(시간 터짐)
- * 방문처리, 2차원 배열에 마킹하기 ? x(공간 터짐)
- * 맵, o
- */
-
 public class Main {
     static int N, T, ans;
-//    static Queue<Node> q = new ArrayDeque<>();
-    static PriorityQueue<Node> q = new PriorityQueue<>();
-//    static Map<Integer, Boolean> map = new LinkedHashMap<>();
     static List<Integer>[] list;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,7 +16,6 @@ public class Main {
             list[i] = new ArrayList<>();
 
         boolean isGame = false;
-        ans = 1 << 30;
 
         for(int i = 0 ; i < N ; i ++){
             st = new StringTokenizer(br.readLine());
@@ -37,16 +27,17 @@ public class Main {
             list[c].add(r);
         }
 
-        if(!isGame)
-            System.out.println(-1);
-        else
-            bfs();
+        if(!isGame) System.out.print(-1);
+        else bfs();
 
-        System.out.println(ans == 1 << 30 ? -1 : ans);
+        System.out.print(ans == 0 ? -1 : ans);
     }
 
     private static void bfs() {
+        PriorityQueue<Node> q = new PriorityQueue<>();
         q.offer(new Node(0, 0, 0));
+
+        List<Integer> next;
 
         while(!q.isEmpty()){
             Node cur = q.poll();
@@ -58,40 +49,20 @@ public class Main {
                 return;
             }
 
-            // 방문 처리
-//            int val = cur.r*10 + cur.c;
-//            map.put(val, true);
-
-            List<Integer> tmp;
-
             for(int nc = c - 2 ; nc <= c + 2 ; nc ++) {
-
                 if(!isValid(nc)) continue;
 
-                tmp = new ArrayList<>();
+                next = list[nc];
 
-//                for(int nr: list[nc]){
-//                    if(Math.abs(nr - r) > 2) continue;
-//
-//                    list[nc].remove(nr);
-//                    q.offer(new Node(nr, nc, cur.d + 1));
-////                    tmp.add(nr);
-//
-//                }
-
-                for (int j = 0; j < list[nc].size(); j++) {
-                    int nr = list[nc].get(j);
-//                    if (r + 2 < nr) break;
-//                    else if (r - 2 > nr) continue;
+                for (int j = 0; j < next.size(); j++) {
+                    int nr = next.get(j);
                     if(Math.abs(nr - r) > 2) continue;
 
-
-                    list[nc].remove(j);
+                    next.remove(j);
                     q.add(new Node(nr, nc, cur.d+1));
                     j--;
                 }
 
-//                for(int val: tmp) list[nc].remove(val);
             }
         }
     }
