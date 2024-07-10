3021b96ff225bdfc68c3d12e82ce87761d814512
giraffe_kim <kyum8562@gmail.com>
2024-07-11 02:03:41 +0900

    [Silver II] Title: 나무 자르기, Time: 484 ms, Memory: 119352 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -2,50 +2,58 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static int N, M;
+    static int N;
     static int[] arr;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int max = 0;
 
-        arr = new int[N+1];
-        int max = Integer.MIN_VALUE;
+        arr = new int[N];
 
         st = new StringTokenizer(br.readLine());
-        for(int i = 1 ; i <= N ; i ++){
-            arr[i] = Integer.parseInt(st.nextToken());
-            max = Math.max(max, arr[i]); // max 값 저장
+        for(int i = 0 ; i < N ; i ++){
+            int tmp = Integer.parseInt(st.nextToken());
+            arr[i] = tmp;
+            max = Math.max(max, tmp);
         }
 
-        System.out.println(binarySearch(max));
+        System.out.print(bs(0, max, M));
     }
 
-    private static int binarySearch(int max) {
-        int s = 0;
-        int e = max;
-        int m = 0; // 자를 나무의 높이 선정
+    private static int bs(int s, int e, int tgt) {
+        int m, ans = 0;
 
         while(e >= s){
             m = (s + e) / 2;
 
-            long res = 0;
-            long cnt = 0;
-            for(int i = 1 ; i <= N ; i ++){
-                if(arr[i] > m){
-                    res += arr[i];
-                    cnt ++;
-                }
-            }
-            res -= cnt * m;
+            // m 일때의 결과 값
+            long res = getResult(m);
 
-            if(res >= M) s = m + 1;
+            // 타겟값보다 크거나 같을 경우
+            if(res >= tgt){
+                s = m + 1;
+                ans = m;
+            }
             else e = m - 1;
         }
-        return e;
+
+        return ans;
+    }
+
+    private static long getResult(int m) {
+        long sum = 0L;
+        long cnt = 0;
+        for(int i = 0 ; i < N ; i ++){
+            if(arr[i] > m){
+                sum += arr[i];
+                cnt ++;
+            }
+        }
+
+        return sum - (cnt * m);
     }
 }
