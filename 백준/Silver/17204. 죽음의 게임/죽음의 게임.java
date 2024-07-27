4e16cea364920c9bbe9da2d05d0fd7fd60a868c9
giraffe_kim <kyum8562@gmail.com>
2024-07-28 02:56:59 +0900

    [Silver III] Title: 죽음의 게임, Time: 108 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/17204. 죽음의 게임/죽음의 게임.java
+++ 백준/Silver/17204. 죽음의 게임/죽음의 게임.java
@@ -1,57 +1,41 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static int[] arr;
+    static boolean[] v;
+    static int cnt, M;
+    static boolean isCheck;
+
     public static void main(String[] args) throws IOException {
-        new Main().solution();
-    }
-
-    static int[] students;
-    static boolean[] visited;
-    static int number = 0;
-    static int bosung;
-    static boolean check = true;
-
-    private void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String[] input = br.readLine().split(" ");
-        int n = Integer.parseInt(input[0]);
-        bosung = Integer.parseInt(input[1]);
-        students = new int[n];
-        for (int i = 0; i < n; i++) {
-            students[i] = Integer.parseInt(br.readLine());
-        }
-        visited = new boolean[students.length];
+        int N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        v = new boolean[N];
+
+        for (int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(br.readLine());
 
         dfs(0);
 
-        if (check) {
-            bw.write(number + "\n");
-        } else {
-            bw.write(-1 + "\n");
-        }
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(!isCheck ? cnt : -1);
     }
 
-    private void dfs(int index) {
-        if (visited[index]) {
-            check = false;
-            return;
-        } else {
-            visited[index] = true;
-            if (index != bosung) {
-                dfs(students[index]);
-                number++;
-            } else {
-                return;
-            }
+    private static void dfs(int cur) {
+        if (v[cur])
+            isCheck = true;
+
+        else {
+            v[cur] = true;
+
+            if(cur == M) return;
+
+            dfs(arr[cur]);
+            cnt ++;
         }
     }
 }
