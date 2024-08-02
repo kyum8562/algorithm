c007d7963a34402c5a45aaf872c553cd34087642
giraffe_kim <kyum8562@gmail.com>
2024-08-02 16:33:46 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 324 ms, Memory: 27288 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -2,36 +2,45 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static StringBuilder sb = new StringBuilder();
+    static int[][] prefixSum;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        int[][] prefixSum = new int[N+1][M+1]; // 패딩
-        for (int i = 1; i <= N; i++) {
+        prefixSum = new int[N+1][M+1];
+
+        for(int i = 1 ; i <= N ; i ++){
             st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= M; j++) {
+            for(int j = 1 ; j <= M ; j ++)
                 prefixSum[i][j] = prefixSum[i][j-1] + Integer.parseInt(st.nextToken());
-            }
         }
-        int tc = Integer.parseInt(br.readLine());
-        while(tc -- > 0){
-            st = new StringTokenizer(br.readLine());
-            int i = Integer.parseInt(st.nextToken());
-            int j = Integer.parseInt(st.nextToken());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
 
-            int sum = 0;
-            for(int a = i ; a <= x ; a ++)
-                sum += prefixSum[a][y] - prefixSum[a][j-1];
-            sb.append(sum).append("\n");
+        for(int i = 1 ; i <= N ; i ++){
+            for(int j = 1 ; j <= M ; j ++)
+                prefixSum[i][j] += prefixSum[i-1][j];
         }
-        System.out.println(sb);
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i = 0 ; i < T ; i ++){
+            st = new StringTokenizer(br.readLine());
+            int r1 = Integer.parseInt(st.nextToken());
+            int c1 = Integer.parseInt(st.nextToken());
+            int r2 = Integer.parseInt(st.nextToken());
+            int c2 = Integer.parseInt(st.nextToken());
+
+            sb.append(calculating(r1, c1, r2, c2)).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+
+    private static int calculating(int r1, int c1, int r2, int c2) {
+        return prefixSum[r2][c2] - prefixSum[r2][c1-1] - prefixSum[r1-1][c2] + prefixSum[r1-1][c1-1];
     }
 }
